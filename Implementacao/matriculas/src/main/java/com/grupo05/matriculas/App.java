5a88498a46a94f923686d35f08530c6db65e11f1
Pedro Barcelos <pbarcelos56@icloud.com>
2022-02-23 22:17:36 -0300

    feat: criacao projeto maven

---
+++ Implementacao/matriculas/src/main/java/com/grupo05/matriculas/App.java
@@ -0,0 +1,13 @@
+package com.grupo05.matriculas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
